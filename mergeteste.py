0c164c3f3e1668e0191090f86f8dc0033aa19bf8
Gustavo Miguel <33351250+Gustavo-Miguel@users.noreply.github.com>
2021-11-04 23:07:36 -0300

    Update mergeteste.py

--- mergeteste.py
+++ mergeteste.py
@@ -4,7 +4,14 @@ import requests
 
 git_url = "https://github.com/Gustavo-Miguel/teste.git"
 pull_url = "https://api.github.com/repos/Gustavo-Miguel/teste/pulls/4"
-repo_dir = "teste2"
+repo_dir = "teste"
 
-cloned_repo = git.Repo.clone_from(git_url, repo_dir)
-print("Cloned Repo!")
+if os.path.exists(repo_dir):
+    cloned_repo = git.Repo.init(repo_dir)
+    #os.remove(repo_dir)
+    print("Directory already exist!")
+else:
+    cloned_repo = git.Repo.clone_from(git_url, repo_dir)
+    print("Directory don't exist!")
+
+print("END")
