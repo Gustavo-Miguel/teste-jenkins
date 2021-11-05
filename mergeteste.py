0dfc745b06f3f80aba9082d350eb463aba851c81
Gustavo Miguel <33351250+Gustavo-Miguel@users.noreply.github.com>
2021-11-05 00:00:00 -0300

    Update mergeteste.py

--- mergeteste.py
+++ mergeteste.py
@@ -13,7 +13,7 @@ def get_description():
 
 if os.path.exists(repo_dir):
     cloned_repo = git.Repo.init(repo_dir)  
-    cloned_repo.head.reset() 
+    cloned_repo.git.stash() 
     print("Directory already exist!")
 else:
     cloned_repo = git.Repo.clone_from(git_url, repo_dir)
