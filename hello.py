b5b53edc870068a0826044cca598d0bf06ba207f
Gustavo Miguel <33351250+Gustavo-Miguel@users.noreply.github.com>
2021-11-04 22:25:25 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("Hello Python.")
+import os
+import git 
+import requests
+
+git_url = "  https://api.github.com/repos/Gustavo-Miguel/teste/pulls/1"
+
+def get_description():
+    description = requests.get(git_url)
+    return description
+
+desc = get_description()
+
+print("---DESCRIPTION---")
+print(desc.json().get('body'))
